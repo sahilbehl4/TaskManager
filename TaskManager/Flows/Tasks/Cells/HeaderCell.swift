26fb530b4e0a17e619ec864cd33bfbd76e4c5254
Sahil Behl <sahilbehl4@ibm.com>
2020-11-26 22:25:53 -0400

    files added

---
+++ TaskManager/Flows/Tasks/Cells/HeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCell.swift
+//  TaskManager
+//
+//  Created by Sahil Behl on 2020-02-11.
+//  Copyright © 2020 sahil. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewHeaderFooterView {
+    @IBOutlet weak var headerLabel: UILabel!
+}
